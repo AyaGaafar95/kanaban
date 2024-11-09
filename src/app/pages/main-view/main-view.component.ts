1d29fe408900a1d93d53cab7400cfc090d38f22e
AyaGaafar95 <ayagafar88@gmail.com>
2024-11-09 16:20:21 +0200

    end drag & drop

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Board } from './../../models/board.model';
+import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -7,7 +9,38 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./main-view.component.scss'],
 
 })
-export class MainViewComponent {
+export class MainViewComponent implements OnInit{
+  constructor(){}
+  board:Board = new Board('test Board',[
+    new Column ('Ideas',[
+      'some rondom idea',
+      'Aya is good in every thing that she do',
+      'this is another rondom idea',
+      'build an awesome application'
+    ]),
+    new Column('Research',[
+
+      'foo',
+      "this was in the 'Reaserch' column",
+      ' how to be very good in front End'
+    ]),
+    new Column('Todo',[
+      'Get to work',
+      'pick up groceries',
+      'Go home',
+      "fall asleep",
+    ]),
+    new Column('Done',[
+      'Get up', 'Brush teeth',
+       'Take a shower',
+       'Check e-mail',
+       'Walk dog']
+
+  )
+  ])
+  ngOnInit(): void {
+
+  }
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
